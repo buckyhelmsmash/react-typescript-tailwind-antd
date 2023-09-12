c909bbe25fbb7cd52b6b661704a46045200110c4
bucky <buckyhelmsmash@gmail.com>
2023-09-12 23:31:03 +0700

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import './App.css';
+
+import {Button} from "antd";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
-  );
+    return (
+        <div className={"flex flex-col gap-4"}>
+            <h1 className="text-3xl font-bold underline text-red-600">
+                Simple React Typescript Tailwind Sample
+            </h1>
+            <Button type={"primary"}>
+                ASD
+            </Button>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './app.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StyleProvider } from '@ant-design/cssinjs';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <StyleProvider hashPriority="high">
+            <App/>
+        </StyleProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
